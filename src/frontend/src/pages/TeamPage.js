15f47efae07e379643380898845883247960c3b0
Ronan <binnyronan@gmail.com>
2021-06-05 15:18:20 +1200

    Setup basic hardcoded fetch Match request

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,12 +1,26 @@
-import { React } from 'react';
+import {React, useEffect, useState} from 'react';
 import {MatchDetailCard} from "../components/MatchDetailCard";
 import {MatchSmallCard} from "../components/MatchSmallCard";
 
-
 export const TeamPage = () => {
+
+    const [team,setTeam] = useState();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch('http://localhost:8080/teams/Delhi%20Capitals');
+                const data = await response.json();
+                setTeam(data);
+            };
+            fetchMatches();
+        },
+        []
+    );
+
     return (
         <div className="TeamPage">
-            <h1>Team Name</h1>
+            <h1>{team.teamName}</h1>
             <MatchDetailCard/>
             <MatchSmallCard/>
             <MatchSmallCard/>
--- src/main/java/com/rrbinny/ipldashboard/controller/TeamController.java
+++ src/main/java/com/rrbinny/ipldashboard/controller/TeamController.java
@@ -3,6 +3,7 @@ package com.rrbinny.ipldashboard.controller;
 import com.rrbinny.ipldashboard.model.Team;
 import com.rrbinny.ipldashboard.repository.MatchRepository;
 import com.rrbinny.ipldashboard.repository.TeamRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
