d3300044bfe1a3b0bb4e3f8eb03b2619f9093f08
Ronan <binnyronan@gmail.com>
2021-06-11 12:13:42 +1200

    Finished home page

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -23,7 +23,7 @@ export const HomePage = () => {
                 <h1 className="app-name">IPL Dashboard</h1>
             </div>
             <div className="team-grid">
-                {teams.map(team => <TeamTile teamName={team.teamName}/>)}
+                {teams.map(team => <TeamTile key={team.id} teamName={team.teamName}/>)}
             </div>
         </div>
     );
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -43,7 +43,7 @@ export const TeamPage = () => {
                 <h3>Latest Matches</h3>
                 <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
             </div>
-            {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}
+            {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} teamName={team.teamName} match={match}/>)}
             <div className="more-link">
                 <Link to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}>More > </Link>
             </div>
